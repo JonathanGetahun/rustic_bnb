9b92eefcec561b0001a4e2bd2199378203da197e
Jonathan Getahun <kgetahun2@gmail.com>
2020-11-09 14:29:28 -0500

    router order

--- server/app.js
+++ server/app.js
@@ -22,6 +22,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/goFindList', listRouter)
+app.use('/customers', bookingRouter)
+app.use('/home', s3Router)
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', function(req,res){
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
@@ -29,8 +31,7 @@ app.get('*', function(req,res){
 app.use('/', signupRouter)
 app.use('/login', loginRouter)
 
-app.use('/customers', bookingRouter)
-app.use('/home', s3Router)
+
 
 
 
