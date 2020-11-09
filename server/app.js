45f10ff38a8e3ea8096612a6dce5239aded01360
Jonathan Getahun <kgetahun2@gmail.com>
2020-11-09 14:22:02 -0500

    added config variables to heroku

--- server/app.js
+++ server/app.js
@@ -21,15 +21,16 @@ mongoose.connect(MONGO_URI
 app.use(cors())
 app.use(express.json())
 
-app.use('/goFindList', listRouter)
-app.use('/customers', bookingRouter)
-app.use('/home', s3Router)
+
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', function(req,res){
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.use('/', signupRouter)
 app.use('/login', loginRouter)
+app.use('/goFindList', listRouter)
+app.use('/customers', bookingRouter)
+app.use('/home', s3Router)
 
 
 
